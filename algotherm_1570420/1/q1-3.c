4abc56a268b1381bebda97b9c1c703e985af428e
hhiroki <gyouzakuuka@gmail.com>
2021-04-24 11:05:45 +0900

    update

---
+++ algotherm_1570420/1/q1-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+    float kashi = 93.45;
+    float ceshi;
+
+    ceshi = (5.0 / 9.0) * (kashi - 32.0);
+    printf("kashi: %f\tceshi: %f\n", kashi, ceshi);
+
+    return 0;
+}
