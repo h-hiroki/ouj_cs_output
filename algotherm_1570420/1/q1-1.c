fc4a1bd94666d959919d4e26c2940e40c140b5c8
hhiroki <gyouzakuuka@gmail.com>
2021-04-24 10:54:35 +0900

    update

---
+++ algotherm_1570420/1/q1-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+    double x = 1.6;
+
+    printf("baseed:  %f\n", x);
+    printf("ceiled:  %f\n", ceil(x));
+    printf("florred: %f\n", floor(x));
+
+    return 0;
+}
